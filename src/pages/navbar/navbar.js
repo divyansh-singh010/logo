7c40de63325bf5e5197d764b25d2ec9ba10b4bbf
Sarthak <122222018+Sarthakagg31@users.noreply.github.com>
2023-12-21 18:28:11 +0530

    Update navbar.js

--- src/pages/navbar/navbar.js
+++ src/pages/navbar/navbar.js
@@ -1,25 +1,35 @@
-import React from "react";
+
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar-container">
-      <div className="navbar">
+      <div className={`navbar ${menuOpen ? "open" : ""}`}>
         <div className="navbar-links">
-          <ul className="navbar-ul">
+          <button className="menu-toggle" onClick={toggleMenu}>
+            Menu
+          </button>
+          <ul className={`navbar-ul ${menuOpen ? "open" : ""}`}>
             <li>
-              <Link to="/" className="nav-link">
+              <Link to="/" className="nav-link" onClick={toggleMenu}>
                 Home
               </Link>
             </li>
             <li>
-              <Link to="/projects" className="nav-link">
+              <Link to="/projects" className="nav-link" onClick={toggleMenu}>
                 Projects
               </Link>
             </li>
             <li>
-              <Link to="/about" className="nav-link">
+              <Link to="/about" className="nav-link" onClick={toggleMenu}>
                 About
               </Link>
             </li>
