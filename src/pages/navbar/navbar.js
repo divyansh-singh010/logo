bca822425fec8c475abfb5b1c3426ef5d63bf56d
Sarthak <122222018+Sarthakagg31@users.noreply.github.com>
2023-12-25 13:02:01 +0530

    Update navbar.js

--- src/pages/navbar/navbar.js
+++ src/pages/navbar/navbar.js
@@ -1,57 +1,32 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import "./navbar.css";
 
-const Navbar = () => {
+import "./Navbar.css";
+import { Link, NavLink } from "react-router-dom";
+
+export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
-    <nav className="navbar-container">
-      <div className={`navbar ${menuOpen ? "open" : ""}`}>
-        <div className="navbar-links">
-          <button
-            className="menu-toggle"
-            onClick={toggleMenu}
-            aria-label="Toggle Menu"
-          >
-            Menu
-          </button>
-          <ul className={`navbar-ul ${menuOpen ? "open" : ""}`}>
-            <li>
-              <Link to="/" className="nav-link" onClick={toggleMenu}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/projects" className="nav-link" onClick={toggleMenu}>
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" className="nav-link" onClick={toggleMenu}>
-                About
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-image">
-          <Link to="/" className="nav-link" onClick={toggleMenu}>
-            <h2 className="logo-text">LOGO</h2>
-            <img
-              src="https://www.w3schools.com/howto/img_avatar.png"
-              alt="logo"
-              className="logo"
-            />
-          </Link>
-        </div>
-        <hr className="navbar-rule" />
+    <nav>
+      <Link to="/" className="title">
+        Website
+      </Link>
+      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
+      <ul className={menuOpen ? "open" : ""}>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/projects">Projects</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
-
-export default Navbar;
